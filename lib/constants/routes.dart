050b6f6dd6d92d6d74e3367f19af5b961e1b1890
Rohit-Kumar-jangid <64152268+Rohit-Kumar-jangid@users.noreply.github.com>
2021-03-25 10:28:57 +0530

    push to master branch

---
+++ lib/constants/routes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:looking_for_job_2/ui/onboarding/signin/signin_screen.dart';
+
+var customRoutes = <String, WidgetBuilder>{
+  '/': (context) => SignInScreen(),
+  '/signIn1': (context) => SignInScreen(),
+/*  '/login_dialogbox': (context) => LoginDialogBox(),
+  '/login_page': (context) => LoginPage(),
+  '/register_page': (context) => RegisterPage(),
+  '/forgot_password': (context) => ForgotPassword(),
+  '/dashboard_screen': (context) => DashBoardScreen(),
+  '/job_description': (context) => JobDescription(),*/
+};
