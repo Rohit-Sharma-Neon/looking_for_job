d7d37858ea1027f28f97b501eb9842b9fe7f2f7f
teamappitronsolution <jaspr.singh4@gmail.com>
2021-04-07 19:33:06 +0530

    push by jaspreet

--- lib/constants/theme/theme_bloc.dart
+++ lib/constants/theme/theme_bloc.dart
@@ -6,6 +6,7 @@ import 'theme_event.dart';
 import 'theme_state.dart';
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
+  ThemeBloc(ThemeState initialState) : super(initialState);
 
   @override
   ThemeState get initialState =>
